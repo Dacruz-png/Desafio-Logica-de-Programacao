c1557e0442cda6c0e34e2ec70874ef64e44fbfbe
Dacruz-png <mateushenriquedacruz880@gmail.com>
2024-10-19 18:30:40 -0300

    desafio atualizado

---
+++ desafio.js
@@ -0,0 +1,104 @@
+// escrevendo o código com switch + cases 
+
+let nome = "Player 01 "
+let xp = 10001
+let nivel1000 = "Ferro"
+let nivel2000 = "Bronze"
+let nivel5000 = "Prata"
+let nivel7000 = "Ouro"
+let nivel8000 = "Platina"
+let nivel9000 = "Ascendente"
+let nivel10000 = "Imortal"
+let nivel10001 = "Radiante"
+
+
+switch(xp) {
+    case 1000:
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivel1000)
+        break
+    case 2000:
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivel2000)
+        break    
+    case 5000:
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivel5000)
+        break
+    case 7000:
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivel7000)
+        break
+    case 8000:
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivel8000)
+        break
+    case 9000:
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivel9000)
+        break
+    case 10000:
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivel10000)
+        break
+    case 10001:
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivel10001)
+        break
+    }
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// outra forma de escrever o código usando if + else
+        
+let nomeJogador = "JOGADOR 001 "
+let xpNivel = 10001
+let nivelFerro = 1000
+let nivelBronze = 2000
+let nivelPrata = 5000
+let nivelOuro = 7000
+let nivelPlatina = 8000
+let nivelAscendente = 9000
+let nivelImortal = 10000
+let nivelRadiante = 10001 
+    
+    if (xp === nivelFerro){
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivelFerro + ", Nivel Ferro" )
+    }else{
+    
+    }
+    
+    if (xp === nivelBronze){
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivelBronze + ", Nivel Bronze")
+    }else{
+    
+    }
+    
+    if (xp === nivelPrata){
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivelPrata + ", Nivel Prata")
+    }else{
+    
+    }
+    
+    if (xp === nivelOuro){
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivelOuro + ", Nivel Ouro")
+    }else{
+    
+    }
+    
+    
+    if (xp === nivelPlatina){
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivelPlatina + ", Nivel Platina")
+    }else{
+    
+    }
+    
+    if (xp === nivelAscendente){
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivelAscendente + ", Nivel Ascendente")
+    }else{
+    
+    }
+    
+    if (xp === nivelImortal){
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivelImortal + ", Nivel Imortal")
+    }else{
+    
+    }
+    
+    if (xp === nivelRadiante){
+        console.log("O Herói de nome " + nome +  "está no nível de " + nivelRadiante + ", Nivel Radiante")
+    }else{
+    
+    }
---
+++ index.js
@@ -0,0 +1,16 @@
+let nome = "Player 01 "
+let xp = 1000
+let nivel1000 = "Ferro"
+let nivel2000 = "Bronze"
+let nivel5000 = "Prata"
+let nivel7000 = "Ouro"
+let nivel8000 = "Platina"
+let nivel9000 = "Ascendente"
+let nivel10000 = "Imortal"
+let nivel10001 = "Radiante"
+
+if (xp <=nivel1000){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivel1000)
+}else{
+    console.log("Treine mais")
+}
