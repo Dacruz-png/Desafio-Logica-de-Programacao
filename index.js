d2e0169fc7d531c65aa96145eb391f4a04a44f94
Dacruz-png <mateushenriquedacruz880@gmail.com>
2024-10-19 18:41:39 -0300

    desafio atualizado

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 let nome = "Player 01 "
-let xp = 1000
-let nivel1000 = "Ferro"
-let nivel2000 = "Bronze"
-let nivel5000 = "Prata"
-let nivel7000 = "Ouro"
-let nivel8000 = "Platina"
-let nivel9000 = "Ascendente"
-let nivel10000 = "Imortal"
-let nivel10001 = "Radiante"
+let xp = 10001
+let nivelFerro = 1000
+let nivelBronze = 2000
+let nivelPrata = 5000
+let nivelOuro = 7000
+let nivelPlatina = 8000
+let nivelAscendente = 9000
+let nivelImortal = 10000
+let nivelRadiante = 10001
 
-if (xp <=nivel1000){
-    console.log("O Herói de nome " + nome +  "está no nível de " + nivel1000)
+if (xp === nivelFerro){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivelFerro + ", Nivel Ferro" )
 }else{
-    console.log("Treine mais")
+
+}
+
+if (xp === nivelBronze){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivelBronze + ", Nivel Bronze")
+}else{
+
+}
+
+if (xp === nivelPrata){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivelPrata + ", Nivel Prata")
+}else{
+
+}
+
+if (xp === nivelOuro){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivelOuro + ", Nivel Ouro")
+}else{
+
+}
+
+
+if (xp === nivelPlatina){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivelPlatina + ", Nivel Platina")
+}else{
+
+}
+
+if (xp === nivelAscendente){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivelAscendente + ", Nivel Ascendente")
+}else{
+
+}
+
+if (xp === nivelImortal){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivelImortal + ", Nivel Imortal")
+}else{
+
+}
+
+if (xp === nivelRadiante){
+    console.log("O Herói de nome " + nome +  "está no nível de " + nivelRadiante + ", Nivel Radiante")
+}else{
+
 }
