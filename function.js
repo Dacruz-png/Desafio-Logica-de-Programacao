ff7a1e2297160647221b0eb5c0f124c7fedbc53e
Dacruz-png <mateushenriquedacruz880@gmail.com>
2024-10-23 18:31:58 -0300

    Projeto Calculadora de Ranking

---
+++ function.js
@@ -0,0 +1,16 @@
+torrar()
+function torrar(){
+    console.log("torrando pão")
+    injetarPão()
+}
+
+//pode chamar função dentro de função
+//não se deve começar funções com números
+//evite nomes com espaço
+//funções são ações coloque como se fosse verbos, nomes que remetem à funções e nomes claros
+
+function injetarPão(){
+    console.log("Preparando para injetar o pão ")
+    console.log("Finalizado")
+
+}
