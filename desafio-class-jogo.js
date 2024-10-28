7a15724e4b6fd16fac62d41b14eb3ba0b961adfd
Dacruz-png <mateushenriquedacruz880@gmail.com>
2024-10-28 17:22:35 -0300

    Em Progresso

--- desafio-class-jogo.js
+++ desafio-class-jogo.js
@@ -21,31 +21,73 @@ class heroType{
 
 
 class hero{
-    constructor(name, age, type){
+    constructor(name, age, tipo){
         this.name = name
         this.age = age
-        this.type = type
+        this.type = tipo
     }
 
     atacar(){
-        console.log(`o ${this.type} atacou usando ${this.espada}`)
+        console.log(`o ${this.guerreiro} atacou usando ${this.espada}`)
     }
 }
 
-class attack{
-    constructor(magia, espada, marciais, shuriken){
-        this.magia = magia
-        this.espada = espada
-        this.marciais = marciais
-        this.shuriken = shuriken
+
+
+
+let ataqueHero = new heroType(`atacou usando espada`, 
+    `atacou usando magia`,
+    `atacou usando marciais`,
+    `atacou usando shuriken`,
+    `atacou usando espada`)
+console.log(ataqueHero)
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class heroes{
+    constructor(name, age, tipo, arma){
+        this.name = name
+        this.age = age
+        this.type = tipo
+        this.arma = arma
     }
 
-
+    atacar(){
+        console.log(`o ${this.type} atacou usando ${this.arma}`)
+    }
 }
 
-let heroName = new hero()
 
-if(attack){
-    this.mago === this.magia
-    console.log("usou magia")
+let HeroHacker = new heroes("Marcos Holloway", 24,"Hacker", "DDOS")
+HeroHacker.atacar()
+
+let HeroGuerreiro = new heroes("Kratos", 30,"Guerreiro", "Espada")
+
+let HeroMago = new heroes("Mestre dos Magos", 89,"Mago", "Magia")
+
+let HeroMonge= new heroes("Dalai Lama", 70,"Monge", "Marciais")
+
+
+let HeroNinja= new heroes("Jack Chan", 55,"Ninja", "Shuriken")
+
+const ataquesDosHeroes = "" 
+
+switch(ataquesDosHeroes){
+    case HeroHacker:
+        console.log(HeroHacker.atacar())
+    break;
+    case HeroGuerreiro:
+        console.log(HeroGuerreiro.atacar())
+    break;
+    case HeroMago:
+        console.log(HeroMago.atacar())
+    break;
+    case HeroMonge:
+        console.log(HeroMonge.atacar())
+    break;
+    case HeroNinja:
+        console.log(HeroNinja.atacar())
+    break;
+    default:
+
 }
